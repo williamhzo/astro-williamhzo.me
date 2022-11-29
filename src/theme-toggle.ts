bc9112377d9d3549b3a63a70d876d5a50ecd11a2
William H <william.hermozo@protonmail.com>
2022-11-29 16:59:04 +0100

    add toggle script

---
+++ src/theme-toggle.ts
@@ -0,0 +1,59 @@
+const THEME_STORAGE_KEY = 'theme-preference';
+
+let preference: string = getColorPreference();
+
+function onToggleClick() {
+  preference = preference === 'light' ? 'dark' : 'light';
+  setColorPreference();
+}
+
+window.onload = () => {
+  applyPreferenceToDom();
+  document
+    .querySelector('#theme-toggle')
+    ?.addEventListener('click', onToggleClick);
+};
+
+function onPrefersColorSchemeChange(event: MediaQueryListEvent) {
+  preference = event.matches ? 'dark' : 'light';
+  setColorPreference();
+}
+
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', onPrefersColorSchemeChange);
+
+/**
+ * PRIVATE HELPERS
+ */
+
+function getColorPreference() {
+  console.log('getting pref...');
+  if (localStorage.getItem(THEME_STORAGE_KEY)) {
+    return localStorage.getItem(THEME_STORAGE_KEY) as string;
+  } else {
+    console.log(
+      'system pref is ',
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+    );
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  }
+}
+
+function applyPreferenceToDom() {
+  document.firstElementChild?.setAttribute('data-theme', preference);
+  document
+    .querySelector('#theme-toggle')
+    ?.setAttribute('aria-label', preference);
+}
+
+function setColorPreference() {
+  localStorage.setItem(THEME_STORAGE_KEY, preference);
+  applyPreferenceToDom();
+}
+
+export {};
