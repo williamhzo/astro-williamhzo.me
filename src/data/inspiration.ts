5aea851d7826eda8f2586090dab005b81e4d8198
William H <william.hermozo@protonmail.com>
2023-01-04 17:03:08 +0100

    extract data objects in dedicated files

---
+++ src/data/inspiration.ts
@@ -0,0 +1,52 @@
+export const DESIGNS = [
+  {
+    title: 'inclusive-components',
+    href: 'https://inclusive-components.design',
+    tags: ['css', 'a11y'],
+  },
+  {
+    title: 'amie.so',
+    href: 'https://amie.so',
+    tags: ['design engineering'],
+  },
+  {
+    title: 'every-layout.dev',
+    href: 'https://every-layout.dev',
+    tags: ['css'],
+  },
+  {
+    title: 'press.stripe',
+    href: 'https://press.stripe.com',
+    tags: ['3D'],
+  },
+  {
+    title: 'rauno.me',
+    href: 'https://rauno.me',
+    tags: ['ui', 'experimental'],
+  },
+  {
+    title: 'linear.app/features',
+    href: 'https://linear.app/features',
+    tags: ['ui'],
+  },
+  {
+    title: 'uiw.tf',
+    href: 'https://uiw.tf',
+    tags: ['ui', 'tools'],
+  },
+  {
+    title: 'danilowoz.com',
+    href: 'https://danilowoz.com',
+    tags: ['typography'],
+  },
+  {
+    title: 'emilkowal.ski',
+    href: 'https://emilkowal.ski',
+    tags: ['ui'],
+  },
+  {
+    title: 'ui.land',
+    href: 'https://ui.land',
+    tags: ['design engineering'],
+  },
+];
---
+++ src/data/links.ts
@@ -0,0 +1,30 @@
+export const LINKS = [
+  {
+    label: 'twitter',
+    href: 'https://twitter.com/williamhzo',
+  },
+  {
+    label: 'email',
+    href: 'mailto:hi@williamhzo.me',
+  },
+  {
+    label: 'github',
+    href: 'https://github.com/williamhzo',
+  },
+  {
+    label: 'read.cv',
+    href: 'https://read.cv/williamhzo',
+  },
+  {
+    label: 'mastodon',
+    href: 'https://mastodon.design/@williamhzo',
+  },
+  {
+    label: 'codepen',
+    href: 'https://codepen.io/williamhzo',
+  },
+  {
+    label: 'linkedin',
+    href: 'https://www.linkedin.com/in/williamhermozo',
+  },
+];
