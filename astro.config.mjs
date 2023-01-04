3aa7d6c748b144df928c5a6ad298c5e830115aff
William H <william.hermozo@protonmail.com>
2023-01-04 14:20:10 +0100

    exclude playground page from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import sitemap from '@astrojs/sitemap';
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   site: 'https://williamhzo.me/',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      // exclude page from the generated sitemap
+      filter: (page) => page !== 'https://williamhzo.me/notes/playground',
+    }),
+  ],
   experimental: {
     contentCollections: true,
   },
