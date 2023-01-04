807ea50bcadbfb5a956596c3596a08351e4a2934
William H <william.hermozo@protonmail.com>
2023-01-04 13:31:22 +0100

    add intellisense to astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+/** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   site: 'https://williamhzo.me/',
   integrations: [sitemap()],
   experimental: {
-    // see https://docs.astro.build/en/guides/content-collections/
     contentCollections: true,
   },
 });
