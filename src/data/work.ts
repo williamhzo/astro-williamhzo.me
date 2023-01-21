4a2eaadbe855bdbad91123944c909bf70e189191
William H <william.hermozo@protonmail.com>
2023-01-21 17:11:00 +0000

    add work page to replace inspiration

---
+++ src/data/work.ts
@@ -0,0 +1,20 @@
+export const WORK = [
+  {
+    title: 'Mansa',
+    href: 'https://getmansa.com/',
+    tags: ['2020-2022'],
+    description: 'First Frontend then Lead Frontend Engineer',
+  },
+  {
+    title: 'Midnight',
+    href: 'https://midnight.io/',
+    tags: ['2020'],
+    description: 'TODO',
+  },
+  {
+    title: 'Zone Pavillonnaire',
+    href: 'https://pavillonnaire.zone/',
+    tags: ['2020'],
+    description: 'TODO',
+  },
+];
