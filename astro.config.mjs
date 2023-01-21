c36f8f56c95a8797015ba4618274d0702fc13144
William H <william.hermozo@protonmail.com>
2023-01-21 11:34:59 +0000

    rm playground

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,7 @@ import prefetch from '@astrojs/prefetch';
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   site: 'https://williamhzo.me/',
-  integrations: [
-    sitemap({
-      // exclude page from the generated sitemap
-      filter: (page) => page !== 'https://williamhzo.me/notes/playground',
-    }),
-    prefetch(),
-  ],
+  integrations: [sitemap(), prefetch()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
